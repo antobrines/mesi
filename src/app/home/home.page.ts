cb47eaebbe3a33589000e4721fe3e47b7e48781f
jayhem10 <noble.jka@gmail.com>
2021-07-25 10:44:48 +0200

    lien home.html vers search cat + ts à copier dans search-cat.ts

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -10,7 +11,14 @@ export class HomePage implements OnInit {
 
   public products = null;
   public categories = null;
-  constructor(private http: HttpClient) { }
+
+  //à coller dans la page search-cat
+  category_id: string;
+
+   //à coller dans la page search-cat
+  constructor(private http: HttpClient,private actRoute: ActivatedRoute) {
+    this.category_id = this.actRoute.snapshot.params.id;
+  }
 
   ngOnInit(): void {
     this.getProducts();
@@ -29,4 +37,10 @@ export class HomePage implements OnInit {
     });
   }
 
+   //à coller dans la page search-cat
+  getCategoriesProduct(){
+    this.http.get('http://127.0.0.1:8000/product/' + this.category_id + '/categories').subscribe((datas: any) => {
+      this.categories = datas.data.data;
+    });
+  }
 }
