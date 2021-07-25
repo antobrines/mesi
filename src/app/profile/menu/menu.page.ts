37e21be483428d7b31fec0dbf0f09680d4be249f
jayhem10 <noble.jka@gmail.com>
2021-07-25 13:32:11 +0200

    add arrow back on profile/home page

--- src/app/profile/menu/menu.page.ts
+++ src/app/profile/menu/menu.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-menu',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuPage implements OnInit {
 
-  constructor() { }
+  constructor(private _location: Location) 
+  {}
+
+  
+  backClicked() {
+    this._location.back();
+  }
 
   ngOnInit() {
   }
