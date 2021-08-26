d586aa565d9493ed985983a531783f8ed388216e
jayhem10 <noble.jka@gmail.com>
2021-08-26 21:59:41 +0200

    add view for ads management

--- src/app/ads/ads-list/ads-list.page.ts
+++ src/app/ads/ads-list/ads-list.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-ads-list',
@@ -6,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ads-list.page.scss'],
 })
 export class AdsListPage implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit() {
