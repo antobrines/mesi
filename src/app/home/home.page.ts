99e589eff3b2558db3bc1307057e04fc354aed3b
jayhem10 <noble.jka@gmail.com>
2021-08-30 10:48:00 +0200

    searchbar on home page + search page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -11,19 +12,32 @@ export class HomePage implements OnInit {
 
   public products = null;
   public categories = null;
+  searchName: any;
+  public items = null;
 
-  //à coller dans la page search-cat
-
-
-   //à coller dans la page search-cat
   constructor(private http: HttpClient,private actRoute: ActivatedRoute) {
-    
-  
   }
 
   ngOnInit(): void {
     this.getProducts();
     this.getCategories();
+  }
+
+  // search products with the search name
+  searchProductByName(key: any) {
+    this.searchName  = key.target.value;
+    if (this.searchName == '') {
+      this.getProductByName("!=!?").subscribe( res => {
+        this.items = res.data.data;
+        console.log(this.searchName);
+      });
+    } else {
+      this.getProductByName(this.searchName).subscribe( res => {
+        this.items = res.data.data;
+        console.log(this.searchName);
+        console.log(this.items);
+      });
+    }
   }
 
   getProducts() {
@@ -38,6 +52,10 @@ export class HomePage implements OnInit {
     });
   }
 
+  getProductByName(name: string) {
+    return this.http.get('http://127.0.0.1:8000/products/search/'+ name).pipe(map((res: any) => {
+        return res;
+    }));
+}
   
-
 }
--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search',
@@ -9,10 +10,31 @@ import { HttpClient } from '@angular/common/http';
 export class SearchPage implements OnInit {
 
   public categories = null;
+  searchName: any;
+  public items = null;
+
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.getCategories();
+  }
+
+
+  // search products with the search name
+  searchProductByName(key: any) {
+    this.searchName  = key.target.value;
+    if (this.searchName == '') {
+      this.getProductByName("!=!?").subscribe( res => {
+        this.items = res.data.data;
+        //console.log(this.products);
+      });
+    } else {
+      this.getProductByName(this.searchName).subscribe( res => {
+        this.items = res.data.data;
+        console.log(this.searchName);
+        console.log(this.items);
+      });
+    }
   }
 
   getCategories() {
@@ -20,5 +42,11 @@ export class SearchPage implements OnInit {
       this.categories = datas.data.data;
     });
   }
+
+  getProductByName(name: string) {
+    return this.http.get('http://127.0.0.1:8000/products/search/'+ name).pipe(map((res: any) => {
+        return res;
+    }));
+}
 
 }
